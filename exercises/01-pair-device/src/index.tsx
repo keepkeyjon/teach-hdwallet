f56cdde4e24d43b06b956631eb49e56511a20a54
keepkeyjon <35975617+keepkeyjon@users.noreply.github.com>
2019-08-24 16:20:34 -0600

    update codesandbox example

--- exercises/01-pair-device/src/index.tsx
+++ exercises/01-pair-device/src/index.tsx
@@ -1,57 +1,14 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-import { Keyring, HDWallet } from "@shapeshiftoss/hdwallet-core";
-import { WebUSBKeepKeyAdapter } from "@shapeshiftoss/hdwallet-keepkey-webusb";
-import { TrezorAdapter } from "@shapeshiftoss/hdwallet-trezor-connect";
-
 import "./styles.css";
-import { string } from "prop-types";
 
-class WalletContext extends React.Component {
-  keyring: Keyring;
-  keepkeyAdapter: any;
-  trezorAdapter: any;
-  wallet: HDWallet;
-
-  constructor(props) {
-    super(props);
-    this.keyring = new Keyring();
-    this.keepkeyAdapter = WebUSBKeepKeyAdapter.useKeyring(this.keyring);
-    this.trezorAdapter = TrezorAdapter.useKeyring(this.keyring, {
-      debug: false,
-      manifest: {
-        appUrl: "example.com",
-        email: "you@example.com"
-      }
-    });
-  }
-
-  async handlePair(vendor: string) {
-    let adapter = {
-      keepkey: this.keepkeyAdapter,
-      trezor: this.trezorAdapter
-    }[vendor];
-
-    console.log(`Pairing ${vendor}`);
-    this.wallet = await adapter.pairDevice();
-    console.log(this.wallet)
-  }
-
-  render() {
-    return (
-      <>
-        <button onClick={() => this.handlePair("keepkey")}>Pair KeepKey</button>
-        <button onClick={() => this.handlePair("trezor")}>Pair Trezor</button>
-      </>
-    );
-  }
-}
+import { Wallet } from "./Wallet";
 
 function App() {
   return (
     <div className="App">
-      <WalletContext />
+      <Wallet />
     </div>
   );
 }
